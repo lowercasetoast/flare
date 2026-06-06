31c08d55f2ded6dbaf7cbe2fc39f8fc22b6e7232
lowercase_toast <dragons011913@gmail.com>
2026-06-06 15:09:36 -0700

    fix studio __init__.py

---
+++ flare/studio/__init__.py
@@ -0,0 +1,7 @@
+from .studio   import Studio
+from .track    import Track, Keyframe
+from .easing   import Easing
+from .objects  import Obj2D, Obj3D
+from .camera   import CameraTrack
+
+__all__ = ["Studio", "Track", "Keyframe", "Easing", "Obj2D", "Obj3D", "CameraTrack"]
